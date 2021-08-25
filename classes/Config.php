ecea49351990eb3f5cce434e93d4b85e5fdff47f
murarik57 <murarikumar57@gmail.com>
2021-08-25 21:44:30 +0530

    Update Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -1,8 +1,8 @@
 <?php
     class Config {
-        const SMTP_HOST = "smtp.elasticemail.com";
-        const SMTP_PORT = 2525;
-        const SMTP_USER = "blackboxpro57@gmail.com";
-        const SMTP_PASSWORD = "036F52CDC9A8B49DBC7F12CBC898BBB61BC0";
+        const SMTP_HOST = 
+        const SMTP_PORT = 
+        const SMTP_USER = 
+        const SMTP_PASSWORD = 
     }
 
