8de9cb4e341f1891257f3b17b0eee77989e77c19
murarik57 <murarikumar57@gmail.com>
2021-08-25 21:47:45 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -3,9 +3,9 @@
 
 <?php   
     define("DB_HOST",'localhost');
-    define("DB_USER",'black');
-    define("DB_PASS",'box');
-    define("DB_NAME",'cms');
+    define("DB_USER",'your user');
+    define("DB_PASS",'your pass');
+    define("DB_NAME",'your db);
 
     $connection=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
     $query="SET NAMES utf8";
